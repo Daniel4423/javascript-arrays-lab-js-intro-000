1da8f973d2b04e35419f39fadefa28f323a91968
Daniel Bailey <daniel4423@gmail.com>
2019-01-30 23:02:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,17 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
-  kitten.push(name);
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
 }
